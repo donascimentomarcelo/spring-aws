f168a68ed6ab5c22bc4b232cc7829b5c43b65e79
Marcelo Sant'Anna <marcelojunin2010@hotmail.com>
2023-03-28 21:04:53 -0300

    Maven to Gradle and removing JMS

--- src/main/java/br/com/springaws/services/ProductServiceImpl.java
+++ src/main/java/br/com/springaws/services/ProductServiceImpl.java
@@ -3,7 +3,6 @@ package br.com.springaws.services;
 
 import br.com.springaws.domain.Product;
 import br.com.springaws.repositories.ProductRepository;
-import br.com.springaws.services.jms.JmsTextMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,27 +12,19 @@ import java.util.List;
 public class ProductServiceImpl implements ProductService {
 
     private ProductRepository productRepository;
-    private JmsTextMessageService jmsTextMessageService;
 
     @Autowired
     public void setProductRepository(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-    @Autowired
-    public void setJmsTextMessageService(JmsTextMessageService jmsTextMessageService) {
-        this.jmsTextMessageService = jmsTextMessageService;
-    }
-
     @Override
     public Product getProduct(Integer id) {
-        jmsTextMessageService.sendTextMessage("Fetching Product ID: " + id );
         return productRepository.findById(id).orElse(null);
     }
 
     @Override
     public List<Product> listProducts() {
-        jmsTextMessageService.sendTextMessage("Listing Products");
         return (List<Product>) productRepository.findAll();
     }
 
